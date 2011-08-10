f5e97877566e423a8798d395aa0ef748b90909af
gmarkall <grm08@doc.ic.ac.uk>
2011-08-10 18:55:22 +0100

    Compiling with read in matrix now.

--- test/main.cpp
+++ test/main.cpp
@@ -1,1 +1,11 @@
 #include "sparse_io.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << "CUDA CG Solver test." << endl;
+
+  csr_matrix<int,double> A = read_csr_matrix<int,double>("inputs/A", true);
+}
