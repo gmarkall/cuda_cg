a7e7a5778093cc30f3c226949aa12a00ccabd4e7
Graham Markall <grm08@doc.ic.ac.uk>
2012-02-16 12:33:23 +0000

    Add code to read in vector.

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,26 @@
 #include "sparse_io.h"
+#include "vector_io.h"
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-  cout << "CUDA CG Solver test." << endl;
+  cout << "CUDA CG Solver test." << endl << endl;
 
-  csr_matrix<int,double> A = read_csr_matrix<int,double>("inputs/A", true);
+  cout << "Reading in matrix" << endl;
+  csr_matrix<int,double> A = read_csr_matrix<int,double>("input/A", true);
+  cout << "Reading in RHS vector" << endl;
+  vec<int,double> b = read_vec<int,double>("input/b");
+  cout << "Reading in reference solution vector" << endl;
+  vec<int,double> x = read_vec<int,double>("input/x");
+
+  // ...
+
+  cout << "Deleting matrix" << endl;
+  delete_csr_matrix(A, HOST_MEMORY);
+  cout << "Deleting RHS vector" << endl;
+  delete_vec(b);
+  cout << "Deleting reference solution vector" << endl;
+  delete_vec(x);
 }
---
+++ test/vector_io.h
@@ -0,0 +1,57 @@
+
+#ifndef VECTOR_IO_H
+#define VECTOR_IO_H
+
+template<typename IndexType, typename ValueType> 
+struct vec {
+  IndexType len;
+  ValueType *val;
+};
+
+template<class IndexType, class ValueType>
+vec<IndexType, ValueType> read_vec(const char *filename)
+{
+  FILE *fd = fopen(filename, "r");
+    
+  if (fd == NULL){
+      printf("Unable to open file %s\n", filename);
+      exit(1);
+  }
+
+  printf("Reading vector from file (%s)\n",filename);
+  
+  IndexType count = 0;
+  if (fscanf(fd, "%%%%Length: %d", &count) != 1)
+  {
+    printf("Error reading vector length.\n");
+    exit(1);
+  }
+  
+  printf("Vector length: %d\n", count);
+
+  ValueType *val = (ValueType*)malloc(sizeof(ValueType)*count);
+
+  for (int i=0; i<count; ++i)
+  {
+    if (fscanf(fd, "%lf\n", &val[i]) != 1)
+    {
+      printf("Error reading value %d\n", i);
+      exit(1);
+    }
+  }
+
+  printf("Finished reading vector.\n");
+
+  vec<IndexType, ValueType> v;
+  v.len = count;
+  v.val = val;
+  return v;
+}
+
+template <class IndexType, class ValueType>
+void delete_vec(vec<IndexType, ValueType> v)
+{
+  free(v.val);
+}
+
+#endif
